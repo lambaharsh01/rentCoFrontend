2f28c5f56c4550c7bd3bf71997cb9f2aae611143
Harsh <118607392+lambaharsh01@users.noreply.github.com>
2024-08-03 23:29:49 +0530

    grouping

--- src/modules/groups/groupsIndex.js
+++ src/modules/groups/groupsIndex.js
@@ -1,12 +1,36 @@
 import Header from "../../components/header";
 import { MdGroupAdd } from "react-icons/md";
 import { useNavigate } from "react-router-dom";
-
+import { toast, ToastContainer } from "react-toastify";
+import axiosInterceptor from "../../utils/axiosInterceptor";
+import { useEffect, useState } from "react";
+// getAllGroups
 export default function GroupsIndex() {
+  const [groups, setGroups] = useState([]);
+
+  useEffect(() => {
+    axiosInterceptor({
+      url: "/api/group/getAllGroups",
+      method: "get",
+    })
+      .then((res) => {
+        setGroups(res?.data?.groups ?? []);
+        toast.success(res.message);
+      })
+      .catch((error) => toast.error(error.message));
+  }, []);
+
   const navigate = useNavigate();
   return (
     <div>
       <Header active="g" />
+
+      <ToastContainer
+        autoClose={2000}
+        hideProgressBar={true}
+        newestOnTop={true}
+      />
+
       <div className="container-fluid">
         <div className="row">
           <div className="col-12 flex justify-end pt-2 pe-4">
