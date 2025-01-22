5dfae440062b42fc463b264cf5443628def000bd
Harsh <118607392+lambaharsh01@users.noreply.github.com>
2025-01-22 21:35:21 +0530

    removed time stamp from analytics

--- src/components/graphs/monthlyRentTrend.jsx
+++ src/components/graphs/monthlyRentTrend.jsx
@@ -50,41 +50,43 @@ export default function MonthlyRentTrends(){
         fetchBaseGraph()
     }, [])
 
-    if(!totalGraph.length) return 
-
-    return <div className="w-100 p-2 mb-2">
-        
-        <div className="w-100 border-1 rounded-md">
-
-        <div className="rentCoRed rounded-t-md text-center py-1 font-bold">
-            Monthly Rent Trends
-        </div>
+    return <>
+        {totalGraph.length && (
+            <div className="w-100 p-2 mb-2">
             
-         <div className="text-center py-1 font-bold my-2">
-            {(totalGraph[0]?.label || "") + " - " + (totalGraph[totalGraph.length - 1]?.label || "")}
-        </div>
-            
-        <ResponsiveContainer width="100%" height={250}>
-            <LineChart
-            data={totalGraph}
-            margin={{ top: 10, right: 20, left: 4, bottom: 28 }}
-            >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="label" />
-                <YAxis />
-                <Tooltip 
-                // trigger="click"
-                formatter={(value, name) => [
-                    value,
-                    formatLabel(name),
-                  ]}/>
-                <Legend formatter={(value) => formatLabel(value)}/>
-                <Line type="monotone" dataKey="totalRent" stroke="#ff7f50" strokeWidth={2} />
-                <Line type="monotone" dataKey="totalRentPaid" stroke="#2ecc71" strokeWidth={2} />
-            </LineChart>
-        </ResponsiveContainer>
+            <div className="w-100 border-1 rounded-md">
 
-    </div>   
-    </div>   
+            <div className="rentCoRed rounded-t-md text-center py-1 font-bold">
+                Monthly Rent Trends
+            </div>
+                
+            <div className="text-center py-1 font-bold my-2">
+                {(totalGraph[0]?.label || "") + " - " + (totalGraph[totalGraph.length - 1]?.label || "")}
+            </div>
+                
+            <ResponsiveContainer width="100%" height={250}>
+                <LineChart
+                data={totalGraph}
+                margin={{ top: 10, right: 20, left: 4, bottom: 28 }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="label" />
+                    <YAxis />
+                    <Tooltip 
+                    // trigger="click"
+                    formatter={(value, name) => [
+                        value,
+                        formatLabel(name),
+                    ]}/>
+                    <Legend formatter={(value) => formatLabel(value)}/>
+                    <Line type="monotone" dataKey="totalRent" stroke="#ff7f50" strokeWidth={2} />
+                    <Line type="monotone" dataKey="totalRentPaid" stroke="#2ecc71" strokeWidth={2} />
+                </LineChart>
+            </ResponsiveContainer>
+
+        </div>   
+        </div> 
+    )}
+    </>    
 
 }
